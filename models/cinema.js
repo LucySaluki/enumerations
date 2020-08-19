3e752e7f57c28e7b015c73fd842d1f5b0ad47a59
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-08-19 14:19:29 +0100

    all test passed

--- models/cinema.js
+++ models/cinema.js
@@ -2,4 +2,44 @@ const Cinema = function (films) {
   this.films = films;
 };
 
+Cinema.prototype.filmTitles = function() {
+  const titles = this.films.map((film) => {
+     return film.title;
+  }); 
+  return titles;
+};
+
+Cinema.prototype.findFilm = function (title) {
+  const film = this.films.find(film => film.title === title);
+  return film;
+};
+
+Cinema.prototype.filterFilms = function (filterItem, property) {
+  const films = this.films.filter(film => film[property] === filterItem);
+  return films;
+};
+
+Cinema.prototype.checkYear = function (year) {
+    const chosenYear =  this.films.some(film => film.year === year);
+    return chosenYear;
+    // const array = this.films;
+    // const filmInYear = (film) => films.year === year;
+    // array.some(filmInYear);
+};
+
+Cinema.prototype.everyFilm = function (duration) {
+  const longFilm = this.films.every((film) => {
+    return film.length > duration;
+  });
+    
+  return longFilm;
+};
+
+Cinema.prototype.runTime = function () {
+    const lenOfFilms = this.films.reduce((total,film) => {
+      return total + film.length;
+    }, 0)
+    return lenOfFilms;
+};
+
 module.exports = Cinema;
